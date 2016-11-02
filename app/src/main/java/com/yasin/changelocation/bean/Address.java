7f6f9e42af04828cd01d2837369f3a80b55fdefc
Yasin Yao <yili270@163.com>
2016-11-02 17:48:12 +0800

    first sync

---
+++ app/src/main/java/com/yasin/changelocation/bean/Address.java
@@ -0,0 +1,18 @@
+package com.yasin.changelocation.bean;
+
+/**
+ * Creator  : Yasin on 2016/11/2.
+ * Email    :  yasin27878@163.com
+ * Desc     : 保存的地址
+ * Impl Step:
+ */
+
+public class Address {
+    private String id;
+    //地址
+    private String name;
+    //经度
+    private String xLocation;
+    //纬度
+    private String yLocation;
+}
