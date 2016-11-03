ac162f70ac2717cc7f93e8312e77c06f28e86d31
Yasin Yao <yili270@163.com>
2016-11-03 19:10:54 +0800

    一些文件

--- app/src/main/java/com/yasin/changelocation/MainActivity.java
+++ app/src/main/java/com/yasin/changelocation/MainActivity.java
@@ -1,5 +1,9 @@
 package com.yasin.changelocation;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,6 +14,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String LOCOTION_ADDRESS = "北京市海淀区苏州街 ";
+    public static final double LOCOTION_LONGITUDE = 116.306169;
+    public static final double LOCATION_LATITUDE = 39.976694;
+    private Location mCurLocation = null;
+    private LocationManager mLocManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mCurLocation = getLocation(this);
+        setLocation();
+        mLocManager.removeTestProvider(LocationManager.GPS_PROVIDER);
+        mLocManager.addTestProvider(LocationManager.GPS_PROVIDER, true, false, false, false, false, false, false, 1, 1);
+        mLocManager.setTestProviderEnabled(LocationManager.GPS_PROVIDER, true);
+        mLocManager.setTestProviderLocation(LocationManager.GPS_PROVIDER, mCurLocation);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +43,16 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    /**
+     * 为Location设置值
+     */
+    private void setLocation() {
+        if (mCurLocation != null) {
+            mCurLocation.setLongitude(LOCOTION_LONGITUDE);
+            mCurLocation.setLatitude(LOCATION_LATITUDE);
+        }
     }
 
     @Override
@@ -49,4 +76,21 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * @param context
+     * @return
+     */
+    public Location getLocation(Context context) {
+        mLocManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        Location location = mLocManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location == null) {
+            location = mLocManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        } else if (location == null) {
+            location = mLocManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+        }
+        return location;
+    }
+
+
 }
